7ae9fb60ae5784d582816f2198c36ce266fa4309
brendansky <33739060+brendansky@users.noreply.github.com>
2018-02-20 16:34:15 -0600

    added character choice functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,92 @@
 $(document).ready(function () {
 
-    var anakin = {
-
-        card : $(".anakin"),
-        health : 100,
-        attack : 25,
-    };
-
-    var obiWan = {
-
-        card : $(".obi-wan"),
-        health : 125,
-        attack : 20,
-    };
 
 
-
-    $(".obi-wan").on("click", ".normal-button", function () {
-     
-        $(".anakin-health").text( anakin.health - obiWan.attack);
-    
-
+    function game() {
         
+        var characterChosen = false
+
+        var anakin = {
+    
+            card: $(".anakin"),
+            health: 100,
+            attack: 25,
+        };
+    
+        var obiWan = {
+    
+            card: $(".obi-wan"),
+            health: 125,
+            attack: 20,
+        };
+
+
+        $(".obi-wan").on("click", ".image", function () {
+            $(".your-character").append(obiWan.card);
+            $(".defender").append(anakin.card);
+            characterChosen = true
+            $(".obi-wan").on("click", ".attack-button", function () {
+                anakin.health -= obiWan.attack
+                $(".anakin-health").text("health: " + anakin.health);
+
+
+            });
+
+            $(".anakin").on("click", ".attack-button", function () {
+                obiWan.health -= anakin.attack
+                $(".obi-wan-health").text("health: " + obiWan.health);
+
+            });
+
+
+        });
+
+        $(".anakin").on("click", ".image", function () {
+            $(".your-character").append(anakin.card);
+            $(".defender").append(obiWan.card);
+            characterChosen = true
+            $(".obi-wan").on("click", ".attack-button", function () {
+                anakin.health -= obiWan.attack
+                $(".anakin-health").text("health: " + anakin.health);
+
+
+            });
+
+            $(".anakin").on("click", ".attack-button", function () {
+                obiWan.health -= anakin.attack
+                $(".obi-wan-health").text("health: " + obiWan.health);
+
+            });
+
+        });
+
+
+    }
+
+    game();
+
+    $(document).on("click", ".restart-button", function () {
+
+        game ();
+
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
