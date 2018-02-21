8dd49f938f96fb34cfcba15fc41f86243efa7af4
brendansky <33739060+brendansky@users.noreply.github.com>
2018-02-20 20:21:49 -0600

    added images for other remaining characters

---
+++ assets/javascript/game1.js
@@ -0,0 +1,97 @@
+$(document).ready(function () {
+
+
+    function game() {
+
+        var characterChosen = false
+
+        var anakin = {
+
+            card: $(".anakin"),
+            health: 100,
+            attack: 25,
+        };
+
+        var obiWan = {
+
+            card: $(".obi-wan"),
+            health: 125,
+            attack: 20,
+        };
+
+        // selects obi-wan as main chatacter //
+        $(".obi-wan").on("click", ".image", function () {
+            if (characterChosen === false) {
+
+                $(".your-character").append(obiWan.card);
+                $(".enemies-remaining").append(anakin.card);
+                characterChosen = true
+            }
+
+
+            $(".obi-wan").on("click", ".attack-button", function () {
+                anakin.health -= obiWan.attack;
+                obiWan.attack += 1;
+                $(".anakin-health").text("health: " + anakin.health);
+
+
+            });
+
+            $(".anakin").on("click", ".attack-button", function () {
+                obiWan.health -= anakin.attack;
+                $(".obi-wan-health").text("health: " + obiWan.health);
+
+            });
+
+
+        });
+        
+        
+        
+        // selects anakin as main character //
+        $(".anakin").on("click", ".image", function () {
+            if (characterChosen === false) {
+                $(".your-character").append(anakin.card);
+                $(".enemies-remaining").append(obiWan.card);
+                characterChosen = true;
+            }
+
+            $(".obi-wan").on("click", ".attack-button", function () {
+                anakin.health -= obiWan.attack;
+                $(".anakin-health").text("health: " + anakin.health);
+
+            });
+
+            $(".anakin").on("click", ".attack-button", function () {
+                obiWan.health -= anakin.attack;
+                $(".obi-wan-health").text("health: " + obiWan.health);
+
+            });
+
+        });
+
+
+    }
+
+    game();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
