eb45dd32ad841e18bdaf8dd8c0d0b94198e0beb3
brendansky <33739060+brendansky@users.noreply.github.com>
2018-02-20 21:58:52 -0600

    created new framework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,38 @@
 $(document).ready(function () {
 
 
-var characterNames = ["anakin", "obi wan kenobi", "mace windu"];
+    var characters = {
+        obiWan: { "name": "obi wan kenobi", "health": 125, "attack": 20, "card": $(".obi-wan") },
+        maceWindu: { "name": "mace windu", "health": 125, "attack": 20, "card": $(".mace-windu") },
+        yoda: { "name": "yoda", "health": 125, "attack": 20 },
+        palpatine: { "name": "palpatine", "health": 125, "attack": 20 },
+        dooku: { "name": "dooku", "health": 125, "attack": 20 },
+        anakin: { "name": "anakin", "health": 125, "attack": 20 }
 
-for (i=0; i < characterNames.length; i++){
-    console.log(characterNames[i]);
-}
+    }
 
+    var characterChosen = false
+
+
+    $.each(characters, function () {
+        $.each(this, function (key, value) {
+            console.log(key + ": " + value)
+        });
+    });
+
+    $(".obi-wan").on("click", ".image", function () {
+        console.log("hello there");
+        $(".character-lobby").removeClass("hidden");
+        $(".your-character").append(characters.obiWan.card);
+
+    });
+
+    $(".mace-windu").on("click", ".image", function () {
+        console.log("hello there");
+        $(".character-lobby").removeClass("hidden");
+        $(".your-character").append(characters.maceWindu.card);
+
+    });
 
 
 
