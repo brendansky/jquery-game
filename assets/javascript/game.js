6e2c279877420d0ba56930048981c1419facd813
brendansky <33739060+brendansky@users.noreply.github.com>
2018-02-21 21:56:00 -0600

    dynamically created images for each character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,50 +2,62 @@ $(document).ready(function () {
 
 
     var characters = {
-        obiWan: { "name": "obi wan kenobi", "health": 125, "attack": 20, "card": $(".obi-wan"), "image": "obi-wan.jpg" },
-        maceWindu: { "name": "mace windu", "health": 125, "attack": 20, "card": $(".mace-windu") },
-        yoda: { "name": "yoda", "health": 125, "attack": 20 },
-        palpatine: { "name": "palpatine", "health": 125, "attack": 20 },
-        dooku: { "name": "dooku", "health": 125, "attack": 20 },
-        anakin: { "name": "anakin", "health": 125, "attack": 20 }
+        obiWan: { "name": "Obi Wan Kenobi", "health": 125, "attack": 20, "image": "obi-wan.jpg", "class": "obi-wan" },
+        maceWindu: { "name": "Mace Windu", "health": 125, "attack": 20, "image": "mace-windu.jpg" },
+        yoda: { "name": "Yoda", "health": 125, "attack": 20, "image": "yoda.jpg" },
+        palpatine: { "name": "Palpatine", "health": 125, "attack": 20, "image": "palpatine.png" },
+        dooku: { "name": "Dooku", "health": 125, "attack": 20, "image": "dooku.jpg" },
+        anakin: { "name": "Anakin", "health": 125, "attack": 20, "image": "anakin.jpg" },
 
     }
 
     var characterChosen = false
 
-    var characterButton = $("<div>")
-
-
-
-
     $.each(characters, function () {
         $.each(this, function (key, value) {
             console.log(key + ": " + value)
+
         });
 
         var characterButton = $("<div>");
-        characterButton.addClass("card");
+        var characterImage = $('<img src="assets/images/' + this.image + '">')
+        characterImage.addClass("image");
+        characterButton.addClass("card col-3");
         characterButton.text(this.name);
-        $(".character-selection").append(characterButton);
+        characterButton.append(characterImage);
+        $(".character-buttons").append(characterButton);
 
+         
+
+        $(".card").on("click", ".image", function () {
+            console.log("hello there");
+    
+            $(".character-selection").addClass("hidden");
+    
+            var characterLobby = $(".character-lobby");
+            characterLobby.removeClass("hidden");
+    
+            $(".your-character").append(characterButton);
+    
+    
+        });
+    
 
     });
 
-    $(".obi-wan").on("click", ".image", function () {
+
+    $(".card").on("click", ".image", function () {
         console.log("hello there");
-        $(".character-lobby").removeClass("hidden");
-        $(".your-character").append(characters.obiWan.card);
+
+        $(".character-selection").addClass("hidden");
+
+        var characterLobby = $(".character-lobby");
+        characterLobby.removeClass("hidden");
+
+        $(".your-character").append(characterButton);
+
 
     });
-
-    $(".mace-windu").on("click", ".image", function () {
-        console.log("hello there");
-        $(".character-lobby").removeClass("hidden");
-        $(".your-character").append(characters.maceWindu.card);
-
-    });
-
-
 
 
 
